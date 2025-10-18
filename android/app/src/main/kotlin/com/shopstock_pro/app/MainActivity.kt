dc3022ace93f4d09d0164695ab315c1c117548a2
sudhanshu-881 <92862248+sudhanshu-881@users.noreply.github.com>
2025-10-19 04:56:17 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/shopstock_pro/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.shopstock_pro.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
