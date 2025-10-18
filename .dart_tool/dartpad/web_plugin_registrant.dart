4fcf40edbf0fe7ff451ace93459e6d3b77875e45
sudhanshu-881 <92862248+sudhanshu-881@users.noreply.github.com>
2025-10-19 04:59:48 +0530

    Create web_plugin_registrant.dart

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,34 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:camera_web/camera_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:image_cropper_for_web/image_cropper_for_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:mobile_scanner/src/web/mobile_scanner_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:speech_to_text/speech_to_text_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  CameraPlugin.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  ImageCropperPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  MobileScannerWeb.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  SpeechToTextPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
